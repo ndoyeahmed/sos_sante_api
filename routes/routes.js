b43cc345ac4e412f85471d54c4630504afee0847
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2019-10-04 11:00:04 +0000

    notification

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const userController = require("../controller/user.controller");
+const notifController = require('../controller/notificationController');
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger");
 const api = "/api";
@@ -16,4 +17,6 @@ module.exports = function (app) {
     app.post(api + "/signin");
 
     app.get(api + "/roles", userController.allRoles);
+
+    app.post(api + "/notification", notifController.notif)
 };
