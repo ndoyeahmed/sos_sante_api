9a2bc2fbe2a98fd592dc2c4dcd0a927ccd16aa3a
abou <abdoulayeciss@rezilux.com>
2019-12-06 10:14:56 +0000

    ChangeApikeyNotification

--- controller/notificationController.js
+++ controller/notificationController.js
@@ -1,15 +1,18 @@
 const FCM = require('fcm-node');
 
 exports.notif = (req, res) => {
-    let fcm = new FCM('AAAAz8MKqP8:APA91bGZjDXBaF5kIt1zgDx9r75e1f6-C3X0i2Td0nDu-N2UseWNALtOJt_USRYck4uS_oyB_2KQWhabdbRnAPQQ28Qow4trdf0KLSiod4YRShg55I7Msu60HxGh8eUSisTY96e7K9mr');
+    let fcm = new FCM('AAAAt49xgek:APA91bGFZUVRs9iPWUigQc3OS9Fx2hCR7ZRVC9SC4C3YybMzOJGU9Qg_Xd1Fhr1iTab7dOAz7Fk9BDiFj3DIcuo2tfe0-c88QjUk5gxJhL-bqd45xRZnCDINrhIo1uUZNQnWXHazbtIv');
 
     let message = {
         to: req.body.device,
         notification: {
             title: req.body.title,
-            body: req.body.body
+            body: req.body.body,
+            sound: "default",
+            click_action: "FCM_PLUGIN_ACTIVITY",
+            icon: "fcm_push_icon"
         },
-        data: {"click_action": "FLUTTER_NOTIFICATION_CLICK", "id": "1", "status": "done"}
+        data: { "click_action": "FLUTTER_NOTIFICATION_CLICK", "id": "1", "status": "done" }
     }
     fcm.send(message, function (err, response) {
         if (err) {
